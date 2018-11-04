345b01de40443ef0ab2305c5bb0843f506d6bf2c
Siddhant Kushwaha <k16.siddhant@gmail.com>
2018-11-04 22:42:02 +0530

    relevance querying

--- lucy_lucene/src/main/java/com/siddhantkushwaha/lucy/Searcher.java
+++ lucy_lucene/src/main/java/com/siddhantkushwaha/lucy/Searcher.java
@@ -33,10 +33,10 @@ public class Searcher {
 
         TopDocs foundDocs = searchInDocuments(query, searcher);
 
-        long endTime   = System.nanoTime();
+        long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
 
-        System.out.printf("Total Results: %s, time taken: %f\n\n", foundDocs.totalHits, totalTime/(1e9));
+        System.out.printf("Total Results: %s, time taken: %f\n\n", foundDocs.totalHits, totalTime / (1e9));
 
         for (ScoreDoc sd : foundDocs.scoreDocs) {
             Document d = searcher.doc(sd.doc);
@@ -57,10 +57,21 @@ public class Searcher {
 
         QueryParser qp = new QueryParser(Writer.PLACE_KEY_ABSTRACT, new StandardAnalyzer());
         qp.setDefaultOperator(QueryParser.Operator.AND);
-        qp.setSplitOnWhitespace(true);
-        qp.setAutoGeneratePhraseQueries(true);
+
+        Query phraseQuery = qp.createPhraseQuery(Writer.PLACE_KEY_ABSTRACT, textToFind, 2);
+        System.out.println(phraseQuery);
+
         Query query = qp.parse(textToFind);
         System.out.println(query);
-        return searcher.search(query, 200);
+
+        BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
+        booleanQueryBuilder.add(phraseQuery, BooleanClause.Occur.SHOULD);
+        booleanQueryBuilder.add(query, BooleanClause.Occur.FILTER);
+
+        BooleanQuery finalQuery = booleanQueryBuilder.build();
+        System.out.println(finalQuery);
+
+        TopDocs result = searcher.search(finalQuery, 200);
+        return result;
     }
 }
